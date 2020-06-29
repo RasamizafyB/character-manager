f082d625a4df5e0046527c0591f7fa564e958ef7
RasamizafyB <rasamizafybryan98@gmail.com>
2020-06-29 11:08:22 +0200

    add fech

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,23 @@
-const  axios  =  require ( 'axios' ) ;
-
 
 const character = async () =>{
+
     const charact = await fetch('https://character-database.becode.xyz/characters');
-    const charactArray = await charact.json;
-    console.log(charactArray);
+    const charactArray = await charact.json();
+    console.log(charactArray);  
     
+    const template = document.getElementById('templ');
+
+    const target = document.getElementById('target');
+    
+            
+    charactArray.forEach((el) => {
+        const charactHTML = template.cloneNode(true).content;
+        charactHTML.querySelector('.bd-placeholder-img card-img-top').xmlns = el.image;
+        charactHTML.querySelector('.text-dark').textContent = el.name;
+        charactHTML.querySelector('.card-text').textContent = el.card-text;
+
+        target.appendChild(charactHTML);
+    }); 
 }
+character();
+
