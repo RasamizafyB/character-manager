5ca033b2a0c060dd536bd88ddbb05307631a23b6
RasamizafyB <rasamizafybryan98@gmail.com>
2020-07-01 13:27:38 +0200

    edit view character

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,39 @@
 
-// const fileUpload = require('fuctbase64');
+// Declaration des variable
 
 let charact;
 let charactArray;
 
+let newName;
+let newShortDescription;
+let newDescription;
+let newCharact;
 
+let file;
+let convertBase64;
+let imgBase64;
+let imgBase64_1;
+
+let buttunDelete;
+let deleteId;
+
+let buttunEdite;
+let editName;
+let editImage;
+let editShortDescription;
+let editDescription;
+let editId;
+
+let body;;
+let characterId = async (id) =>await (await fetch("https://character-database.becode.xyz/characters/" + id)).json();
 
 const character = async () =>{
-
     charact = await fetch('https://character-database.becode.xyz/characters');
     charactArray = await charact.json();
-    //console.log(charactArray);  
-    
+    //console.log(charactArray);     
     const template = document.getElementById('templ');
-       
     charactArray.forEach((el) => {
-
         document.querySelector(".row").innerHTML += 
-
             `<div class="col-md-4">
                 <div class="card mb-4 shadow-sm" id="${el.id}">
                     <div class="bd-placeholder-img card-img-top"> 
@@ -27,63 +43,58 @@ const character = async () =>{
                             </div>
                     </div>
                     <div class="card-body">
-                            <h1 class="text-dark">${el.name}</h1>
-                            <p class="card-text">${el.shortDescription}</p>
+                        <h1 class="text-dark">${el.name}</h1>
+                        <p class="card-text">${el.shortDescription}</p>
                     <div class="d-flex justify-content-between align-items-center">
                     <div class="button">
-                    <button type="button" class="btn btn-sm btn-outline-secondary edit" data-toggle="modal" data-target="#editCharacter">Edit</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary delete1" data-toggle="modal" data-target="#deleteCharacter">Delete</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary view" data-toggle="modal" data-target="#viewCharacter">View</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary edit" data-toggle="modal" data-target="#editCharacter" >Edit</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary delete1" data-toggle="modal" data-target="#deleteCharacter">Delete</button>
                     </div>
                 </div>
               </div>
             </div>
           </div>`
+        
     }); 
 }
 character().then(() => {
     Delete();
 });
 
-
-let reader;
-let result;
-let result1;
-
 function encodeImageFileAsURL(element) {
-    var file = element.files[0];
-    reader = new FileReader();
-    reader.onloadend = function() {
-        result = reader.result;
-        result1 = result.substring(23, result.length)
-      console.log('RESULT', result1)
+    file = element.files[0];
+    convertBase64 = new FileReader();
+    convertBase64.onloadend = function() {
+        imgBase64 = convertBase64.result
+                .replace('data:', '')
+                .replace(/^.+,/, '');
+    console.log('RESULT', imgBase64)
     }
-    reader.readAsDataURL(file);
+    convertBase64.readAsDataURL(file);
   }
 
 document.getElementById('add-charact').addEventListener('click', function(){
-    
-    let newName = document.getElementById('recipient-name').value;
-    let newShortDescription = document.getElementById('recipient-shortDescription').value;
-    let newDescription = document.getElementById('message-description').value
-
+    newName = document.getElementById('recipient-name').value;
+    newShortDescription = document.getElementById('recipient-shortDescription').value;
+    newDescription = document.getElementById('message-description').value
     if(newName == false || newShortDescription == false || newDescription == false){
         alert('Complet form please'); 
     }else{
         addCharact();
     }
     async function createCharact() {
-        let newCharact = {
+        newCharact = {
             description : newDescription,
             shortDescription : newShortDescription,
             name : newName,
-            image : result1,
-        }
+            image : imgBase64,
+        } 
         charactArray.push(newCharact);
         console.log(charactArray);
         
         return newCharact;
     };
-
     async function addCharact() {
         const pushCharact = await fetch("https://character-database.becode.xyz/characters", {
             method: "POST",
@@ -97,63 +108,82 @@ document.getElementById('add-charact').addEventListener('click', function(){
     };
 });
 
-    let buttunDelete;
+function Delete(){
+    buttunDelete = document.getElementsByClassName('delete1');
+    //console.log(buttunDelete);
+        for(let i = 0; i < buttunDelete.length; i++){
+            buttunDelete[i].addEventListener('click', async (e) => {
+             
+                deleteId = e.target.parentElement.parentElement.parentElement.parentElement.id
+                //console.log(i);
+               
+                document.getElementById('confirm').addEventListener('click', async function(){
+                    await fetch("https://character-database.becode.xyz/characters/" + deleteId, {
+                        method: "DELETE"
+                    })
+                    document.location.reload();
+               });
 
-    function Delete(){
+            })      
+        }         
+}
+Delete();
 
-        buttunDelete = document.getElementsByClassName('delete1');
-        //console.log(buttunDelete);
-            for(let i = 0; i < buttunDelete.length; i++){
-                buttunDelete[i].addEventListener('click', async (e) => {
-                 
-                    let deleteId = e.target.parentElement.parentElement.parentElement.parentElement.id
-                   
-                    document.getElementById('confirm').addEventListener('click', async function(){
-                        await fetch("https://character-database.becode.xyz/characters/" + deleteId, {
-                            method: "DELETE"
-                        })
-                        document.location.reload();
-                   });
-                })      
-            }         
-    }
-    Delete();
+function Edite(){
+    editName = document.getElementById('editName');
+    editShortDescription = document.getElementById('editShortDescription');
+    editDescription = document.getElementById('editDescription');
+    //editImage = document.getElementById('editFile');
+    body = document.querySelector('body');
+    body.addEventListener("click", async function (e) {
+        if (e.target.classList.contains("edit")) {
+           // let indexFromButtonId = e.target.getAttribute("data-edit")
+            editId = e.target.parentElement.parentElement.parentElement.parentElement.id
+            console.log(editId);
+            let IdCharacter = await characterId(editId)
+            //console.log(IdCharacter); 
+            editName.value = IdCharacter.name;
+            editDescription.value = IdCharacter.description;
+            editShortDescription.value = IdCharacter.shortDescription;
+            //editImage.src =  `data:image/jpeg;base64,${IdCharacter.image}`
+            document.getElementById('editCharact').addEventListener('click', async function(){
+                await fetch("https://character-database.becode.xyz/characters/" + editId, {
+                        method: "PUT",
+                        headers: new Headers({
+                            "Content-type": "application/json; charset=UTF-8",
+                        }),
+                        body: JSON.stringify({
+                            name: editName.value,
+                            description: editDescription.value,
+                            shortDescription: editShortDescription.value,
+                            image : imgBase64,
+                          }),
+                })
+                document.location.reload();
+           });
+        }
+    });     
+}
+Edite();
 
-    let buttunEdite;
-
-    function Edite(){
-        let editName = document.getElementById('editName');
-        let editShortDescription = document.getElementById('editShortDescription');
-        let editDescription = document.getElementById('editDescription');
-        // let newCharact = {
-        //     description : editDescription = el.description,
-        //     shortDescription : editShortDescription,
-        //     name : editName,
-        //     image : result1,
-        // }
-
-        buttunEdite = document.getElementsByClassName('edit');
-        console.log(buttunEdite);
-            for(let x = 0; x < buttunEdite.length; x++){
-                buttunEdite[x].addEventListener('click', async (e) => {
-                  
-                    let editId = e.target.parentElement.parentElement.parentElement.parentElement.id
-                   
-                    editName.value = charactArray[0].name;
-                    editDescription.value = charactArray[0].description;
-                    editShortDescription.value = charactArray[0].shortDescription;
-
-
-                //     document.getElementById('confirm').addEventListener('click', async function(){
-                //         await fetch("https://character-database.becode.xyz/characters/" + editId, {
-                //             method: "PUT"
-                           
-                //         })
-                //         document.location.reload();
-                //    });
-                })      
-            }         
-    }
-    Edite();
+function viewCharact(){
+    viewName = document.getElementById('viewName');
+    viewShortDescription = document.getElementById('viewShortDescription');
+    viewDescription = document.getElementById('viewDescription');
+    body = document.querySelector('body');
+    body.addEventListener("click", async function (e) {
+        if (e.target.classList.contains("view")) {
+            editId = e.target.parentElement.parentElement.parentElement.parentElement.id
+            console.log(editId);
+            let IdCharacter = await characterId(editId)
+            //console.log(IdCharacter); 
+            viewName.innerHTML = IdCharacter.name;
+            viewDescription.innerHTML = IdCharacter.description;
+            viewShortDescription.innerHTML = IdCharacter.shortDescription;
+        };
+    })
+        
+}
+viewCharact();
 
 
