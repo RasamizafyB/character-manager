c53abbc74b69d91985c66a15d1ab57353b3d8528
RasamizafyB <rasamizafybryan98@gmail.com>
2020-07-02 15:46:00 +0200

    add comment

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,30 @@
+// Declaration des variable
+
+    //Get character on api
 
 let charact;
 let charactArray;
+
+    //add character
 
 let newName;
 let newShortDescription;
 let newDescription;
 let newCharact;
 
+    //convert image to base64
+
 let file;
-let reader;
-let result;
+let convertBase64;
+let imgBase64;
+let imgBase64_1;
+
+    //delete character
 
 let buttunDelete;
 let deleteId;
 
-let IdCharacter;
+    //edit character
 
 let buttunEdite;
 let editName;
@@ -23,147 +33,115 @@ let editShortDescription;
 let editDescription;
 let editId;
 
+    //view single character
+
 let viewName;
 let viewShortDescription;
 let viewDescription;
 let viewImage;
 
+    //for the button edit and view
+
 let body;
+
+    //get id character on api
+
+let IdCharacter;
 let characterId = async (id) =>await (await fetch("https://character-database.becode.xyz/characters/" + id)).json();
 
-const API_url =  async function (){
+//display the characters on HTML
 
+const API_url =  async function (){
     charact= await fetch ('https://character-database.becode.xyz/characters');
-    charactArray= await charact.json();
-    
-    console.log(charactArray);
-  
+    charactArray= await charact.json();  
     charactArray.forEach((element, ) => {
         document.querySelector(".row").innerHTML += `
         <div class="col-lg-4 offset-xs-1" id="${element.id}">
-        
-        
-       <div class="our-team-main">
-       
-       
-       <div class="team-front">
-       <img src="data:image;base64,${element.image}"  class="img-fluid" id="img1">
-       <h3 id="name1">${element.name}</h3>
-       <p id="short1">${element.shortDescription}</p>
-       </div>
-       
-       
-       <div class="team-back">
-       <div class="button" style="right:95px;">
-       <button type="button" class="btn btn-sm btn-outline-secondary view" data-toggle="modal" data-target="#viewCharacter">View</button>
-       <button type="button" class="btn btn-sm btn-outline-secondary edit" data-toggle="modal" data-target="#editCharacter">Edit</button>
-       <button type="button" class="btn btn-sm btn-outline-secondary delete1" data-toggle="modal" data-target="#deleteCharacter">Delete</button>
-       </div>
-       
-       <span id="description1">
-       ${element.description}
-       </span>
-       
-       </div>
-       
-        
-       
-       </div>
-       
-       </div>`
-           
+            <div class="our-team-main">    
+                <div class="team-front">
+                    <img src="data:image;base64,${element.image}"  class="img-fluid" id="img1">
+                    <h3 id="name1">${element.name}</h3>
+                    <p id="short1">${element.shortDescription}</p>
+                </div>   
+                <div class="team-back">
+                    <div class="button" style="right:95px;">
+                        <button type="button" class="btn btn-sm btn-outline-secondary view" data-toggle="modal" data-target="#viewCharacter">View</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary edit" data-toggle="modal" data-target="#editCharacter">Edit</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary delete1" data-toggle="modal" data-target="#deleteCharacter">Delete</button>
+                    </div>  
+                    <span id="description1">
+                        ${element.description}
+                    </span> 
+                </div> 
+            </div>     
+       </div>`         
     });
 };
-
 API_url().then(() => {
     Delete();
 });
+
+//change the image to base64
     
 function encodeImageFileAsURL(element) {
     file = element.files[0];
-    reader = new FileReader();
-    reader.onloadend = function() {
-        result = reader.result
-        .replace('data:', '')
-        .replace(/^.+,/, '');
-      console.log('RESULT', result);
+    convertBase64 = new FileReader();
+    convertBase64.onloadend = function() {
+    imgBase64_1 = convertBase64.result;
+    imgBase64 = convertBase64.result
+            .replace('data:', '')
+            .replace(/^.+,/, '');
     }
-    reader.readAsDataURL(file);
-  }
-    
-    
+    convertBase64.readAsDataURL(file);
+}
 
-  
-  
-  document.getElementById('add-charact').addEventListener('click', function(){
+//Add a new character
 
+function Add(){
+    newName = document.getElementById('recipient-name');
+    newShortDescription = document.getElementById('recipient-shortDescription');
+    newDescription = document.getElementById('message-description');
+    document.getElementById('add-charact').addEventListener('click', async function(){
+        if(newName == false || newShortDescription == false || newDescription == false){
+            alert('Complet form please'); 
+        }else{
+            await fetch("https://character-database.becode.xyz/characters", {
+                method: "POST",
+                headers: new Headers({
+                    "Content-Type": "application/json"
+                }),
+                body: JSON.stringify({
+                    description : newDescription.value,
+                    shortDescription : newShortDescription.value,
+                    name : newName.value,
+                    image : imgBase64,
+                }),
+            });
+            document.location.reload();
+        }      
+    });
+}
+Add();
 
-    
-
-    newName = document.getElementById('recipient-name').value;
-    newShortDescription = document.getElementById('recipient-shortDescription').value;
-    newDescription = document.getElementById('message-description').value;
-
-
-    if(newName == false || newShortDescription == false || newDescription == false){
-        alert('Complet form please'); 
-    }else{
-        addCharact();
-    }
-    function createCharact() {
-        
-         newCharact = {
-            description : newDescription,
-            shortDescription : newShortDescription,
-            name : newName,
-            image : result,
-        }
-       
-        
-       charactArray.push(newCharact);
-       console.log(charactArray);
-        
-        
-        return newCharact;
-    };
-
-     
-    async function addCharact() {
-        const pushCharact = await fetch("https://character-database.becode.xyz/characters", {
-            method: "POST",
-            headers: new Headers({
-                "Content-Type": "application/json"
-            }),
-            body: JSON.stringify(await createCharact()),
-        });
-        document.location.reload();
-        return pushCharact;
-    };
-    
-       
-   
-});
-
-
+//Delete a character
 
 function Delete(){
     body = document.querySelector('body');
     body.addEventListener("click", async function (e) {
         if (e.target.classList.contains("delete1")) {
-             
             deleteId = e.target.parentElement.parentElement.parentElement.parentElement.id
-            //console.log(deleteId);
-                document.getElementById('confirm').addEventListener('click', async function(){
-                    await fetch("https://character-database.becode.xyz/characters/" + deleteId, {
-                        method: "DELETE"
-                    })
-                    document.location.reload();
-               });
+            document.getElementById('confirm').addEventListener('click', async function(){
+                await fetch("https://character-database.becode.xyz/characters/" + deleteId, {
+                    method: "DELETE"
+                })
+                document.location.reload();
+            });
         };     
     });         
 }
 Delete();
 
+//Edit an existing character
 
 function Edite(){ 
     editShortDescription = document.getElementById('editShortDescription');
@@ -173,29 +151,24 @@ function Edite(){
     body = document.querySelector('body');
     body.addEventListener("click", async function (e) {
         if (e.target.classList.contains("edit")) {
-           // let indexFromButtonId = e.target.getAttribute("data-edit")
             editId = e.target.parentElement.parentElement.parentElement.parentElement.id
-            //console.log(editId);
             IdCharacter = await characterId(editId)
-            //console.log(IdCharacter); 
             editName.value = IdCharacter.name;
             editDescription.value = IdCharacter.description;
             editShortDescription.value = IdCharacter.shortDescription;
             editImage.src = "data:image/jpeg;base64," + IdCharacter.image;
-            //console.log(editImage);
-            
             document.getElementById('editCharact').addEventListener('click', async function(){
                 await fetch("https://character-database.becode.xyz/characters/" + editId, {
-                        method: "PUT",
-                        headers: new Headers({
-                            "Content-type": "application/json; charset=UTF-8",
-                        }),
-                        body: JSON.stringify({
-                            name: editName.value,
-                            description: editDescription.value,
-                            shortDescription: editShortDescription.value,
-                            image : result || IdCharacter.image,
-                          }),
+                    method: "PUT",
+                    headers: new Headers({
+                        "Content-type": "application/json; charset=UTF-8",
+                    }),
+                    body: JSON.stringify({
+                        name: editName.value,
+                        description: editDescription.value,
+                        shortDescription: editShortDescription.value,
+                        image : imgBase64 || IdCharacter.image,
+                    }),
                 })
                 document.location.reload();
            });
@@ -204,6 +177,7 @@ function Edite(){
 }
 Edite();
 
+//View the select character
 
 function View(){ 
     viewName = document.getElementById('viewName');
@@ -213,16 +187,12 @@ function View(){
     body = document.querySelector('body');
     body.addEventListener("click", async function (e) {
         if (e.target.classList.contains("view")) {
-           // let indexFromButtonId = e.target.getAttribute("data-edit")
             editId = e.target.parentElement.parentElement.parentElement.parentElement.id
-            //console.log(editId);
             IdCharacter = await characterId(editId)
-            //console.log(IdCharacter); 
             viewName.innerHTML = IdCharacter.name;
             viewDescription.innerHTML = IdCharacter.description;
             viewShortDescription.innerHTML = IdCharacter.shortDescription;
             viewImage.src = "data:image/jpeg;base64," + IdCharacter.image;
-            //console.log(editImage);
         }
     }); 
 }    
